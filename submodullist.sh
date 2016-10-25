857784443747b37e2df159779941c326c3f2a661
Philipp Harb <harb@student.tugraz.at>
2016-10-25 14:39:14 +0200

    removed submodules list

--- submodullist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# this is not necessary when git repo was downloaded
-
-SCRIPTDIR=${0%/*} # get dirname
-
-git submodule add https://github.com/tmux-plugins/tmux-resurrect $SCRIPTDIR/tmux
