385779e43339e1fd607698c1c5825f7e2dd52011
Philipp Harb <harb@student.tugraz.at>
2016-10-26 02:17:48 +0200

    vimrc starting the madness

--- linkConfig.sh
+++ linkConfig.sh
@@ -4,6 +4,11 @@
 
 SCRIPTDIR=${0%/*} # get dirname
 
+# update submodules
+cd $SCRIPTDIR
+git submodule update --init --recursive
+git submodule update --recursive --remote
+
 # tmux
 rm ~/.tmux.conf
 rm -r ~/.tmux
@@ -17,8 +22,10 @@ rm ~/.vimrc
 ln -s $SCRIPTDIR/vimrc ~/.vimrc
 #ln -s $SCRIPTDIR/vim ~/.vim
 
-# update submodules
-cd $SCRIPTDIR
-git submodule update --init --recursive
-git submodule update --recursive --remote
+# vim pathogen
+mkdir -p $SCRIPTDIR/vim/autoload
+ln -s $SCRIPTDIR/vim/vim-pathogen/autoload/pathogen.vim $SCRIPTDIR/vim/autoload
 
+# vim monokai
+mkdir -p $SCRIPTDIR/vim/colors
+ln -s $SCRIPTDIR/vim/vim-monokai/colors/monokai.vim $SCRIPTDIR/vim/colors
--- vimrc
+++ vimrc
@@ -1,8 +1,26 @@
 " autoload bundles
-" execute pathogen#infect()
+ execute pathogen#infect()
 
 " start with syntax
 syntax on
 
+# monokai style
+colorscheme monokai
+set t_Co=256  " vim-monokai now only support 256 colours in terminal.
+
 " start with line numbers
 set nu
+
+" old vi legacy disable enter the current millenium https://www.youtube.com/watch?v=XA2WjJbmmoM
+" set nocompatible
+
+" built in file browsing https://www.youtube.com/watch?v=XA2WjJbmmoM
+filetype plugin on
+
+" enable mouse
+" set mouse=a
+
+" bundle airline activate
+let g:airline#extensions#tabline#enabled = 1
+set laststatus=2
+
