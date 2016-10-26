fec3c381624c7dd821a98119d3b0aa184ec7d46d
Philipp Harb <harb@student.tugraz.at>
2016-10-26 15:29:51 +0200

    linkConfigs with less error output (rm - file did not exist...)

--- linkConfig.sh
+++ linkConfig.sh
@@ -2,30 +2,32 @@
 
 # echo "updateLinkConfigs";bash ~/Documents/Git/configs/linkConfig.sh
 
-SCRIPTDIR=${0%/*} # get dirname
+SCRIPTDIR=$(pwd -P)"/"
 
 # update submodules
 cd $SCRIPTDIR
+git pull
 git submodule update --init --recursive
 git submodule update --recursive --remote
 
 # tmux
-rm ~/.tmux.conf
-rm -r ~/.tmux
-mkdir -p $SCRIPTDIR/tmux
+rm ~/.tmux.conf || true
+rm -r ~/.tmux || true
 ln -s $SCRIPTDIR/tmux.conf ~/.tmux.conf
 ln -s $SCRIPTDIR/tmux ~/.tmux
 
 # vim
-rm ~/.vimrc
-rm -r ~/.vim
+rm ~/.vimrc || true
+rm -r ~/.vim || true
 ln -s $SCRIPTDIR/vimrc ~/.vimrc
 ln -s $SCRIPTDIR/vim ~/.vim
 
 # vim pathogen
 mkdir -p $SCRIPTDIR/vim/autoload
+rm $SCRIPTDIR/vim/autoload/pathogen.vim || true
 ln -s $SCRIPTDIR/vim/vim-pathogen/autoload/pathogen.vim $SCRIPTDIR/vim/autoload
 
 # vim monokai
 mkdir -p $SCRIPTDIR/vim/colors
+rm $SCRIPTDIR/vim/colors/monokai.vim || true
 ln -s $SCRIPTDIR/vim/vim-monokai/colors/monokai.vim $SCRIPTDIR/vim/colors
